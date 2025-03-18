023d8db7ccb3abc411165af603d30a7b94d0e31b
Mayanka11y <mayankraj418890@gmail.com>
2025-03-18 11:51:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Get references to the paragraph and button elements
+const statusParagraph = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
+
+// Add a click event listener to the button
+enterButton.addEventListener("click", () => {
+  // Change the content of the paragraph to "Entered Metaverse" wrapped in an <h1> tag
+  statusParagraph.innerHTML = "<h1>Entered Metaverse</h1>";
+});
